8ba4c888e955aded7aa7098cbe4ee32a4bb4ab7b
pammu453 <111453798+pammu453@users.noreply.github.com>
2024-06-11 19:27:40 +0530

    Duplicate files removed

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB=() =>{
-    mongoose.connect(process.env.MONGO_URL)
-    .then(() => console.log('Connected to database'))
-    .catch(err => console.log(err.message));
-}
-
-export default connectDB
